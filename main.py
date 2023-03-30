0f6556b90cbc0ab728457d15d49797aada7a3f99
bahati <bulozealfani@gmail.com>
2023-03-29 23:28:22 -0500

    verify webhook

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ app = Flask(__name__)
 
 access_token = "<access_token>"
 verify_token = "<verify_token>"
+
+@app.route('/', methods=['GET'])
+def verify():
+    # Verify webhook
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == verify_token:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
